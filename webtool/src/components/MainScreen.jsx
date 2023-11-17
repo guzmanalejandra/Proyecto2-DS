65fff7b0a783c370845be10f01527fab03c96d1b
Jorge Caballeros Pérez <60406863+JorgeCab2711@users.noreply.github.com>
2023-11-17 14:23:43 -0600

    webpage

---
+++ webtool/src/components/MainScreen.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+export default function MainScreen() {
+  return (
+    <MyContainer>
+      <MyHeader>UVG Medical AI assistance</MyHeader>
+      <MyForm>
+        <label for="myfile">Select an image iniciate calculation:????</label>
+        <input type="file" id="myfile" name="myfile" multiple/>
+        <input type="submit"/>
+      </MyForm>
+      <ContentContainer></ContentContainer>
+    </MyContainer>
+  )
+}
+
+// God container
+const MyContainer = styled.div`
+    display: grid;
+    gap: 5px;
+    place-items: center;
+    height: 100vh;
+    grid-template-rows: 10% 30% 60% ;
+`
+
+const MyHeader = styled.h1`
+  display: grid;
+  place-items: center;
+  color: white;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  width: 95%;
+  height: 100%;
+  background-color: skyblue;
+  border-radius: 10px;
+`
+
+const ContentContainer = styled.div`
+  background-color: red;
+`
+
+const MyForm = styled.form`
+  display : grid;
+  width: 50%;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  gap: 10px;
+  grid-template-rows: 1fr 1fr 1fr;
+`
